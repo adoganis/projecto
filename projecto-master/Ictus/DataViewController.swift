e2be1120000206b2a22864003b2bc565993abf5e
bobcatkiller12 <36898024+bobcatkiller12@users.noreply.github.com>
2018-06-28 11:48:01 -0400

    Add files via upload
    i hope this works.

---
+++ projecto-master/Ictus/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Ictus
+//
+//  Created by Alexandros Doganis on 6/18/18.
+//  Copyright © 2018 Alexandros Doganis. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
